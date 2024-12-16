d463122195a72591dd8a3e0011ad6a9b6a4c8279
falelu Rahman <tpfalelurahman@gmail.com>
2024-12-16 22:23:35 +0530

    1

---
+++ component/home/ImageSlider.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css"; // Core Swiper styles
+import "swiper/css/pagination"; // Pagination module styles
+import "swiper/css/autoplay"; // Autoplay module styles
+import { Autoplay, Pagination } from "swiper";
+
+const ImageSlider = () => {
+  const images = [
+    "/image/301.jpg",
+    "/image/505.jpg",
+    "/image/712.jpg",
+    "/image/300.jpg",
+  ];
+
+  return (
+    <div className="w-full flex flex-col items-center bg-orange-50 overflow-hidden pb-10">
+      <Swiper
+        modules={[Autoplay, Pagination]}
+        spaceBetween={30}
+        slidesPerView={1}
+        autoplay={{ delay: 3000, disableOnInteraction: false }}
+        pagination={{ clickable: true }}
+        loop={true}
+        className="w-[85%] rounded-xl "
+      >
+        {images.map((image, index) => (
+          <SwiperSlide key={index}>
+            <img
+              src={image}
+              alt={`Slide ${index + 1}`}
+              className="w-full h-auto rounded-xl"
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default ImageSlider;
+
+
+
+
+
+// "use client";
+
+// import React, { useState, useEffect } from "react";
+
+// const ImageSlider = () => {
+//   const images = [
+//     "/image/301.jpg",
+//     "/image/505.jpg",
+//     "/image/712.jpg",
+//   ]; 
+
+//   const [currentIndex, setCurrentIndex] = useState(0);
+
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+//     }, 3000); // Change slide every 3 seconds
+
+//     return () => clearInterval(interval); // Clean up interval on unmount
+//   }, [images.length]);
+
+//   return (
+//     <div className="w-full flex flex-col items-center bg-orange-50 overflow-hidden pb-10">
+//       <img src={images[currentIndex]}
+//         className="bg-cover w-[85%] bg-center transition-all duration-500 rounded-xl"
+//         style={{ backgroundImage: `url(${images[currentIndex]})` }}
+//       ></img>
+      
+//     </div>
+//   );
+// };
+
+// export default ImageSlider;
+
+
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
   content: [
@@ -9,14 +10,43 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Gilroy', ...fontFamily.sans], // Use Gilroy as the default sans-serif font
+        gilroy: ['Gilroy', 'sans-serif'], // Add a custom Gilroy font
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      borderRadius: {
+        '8xl': '4rem',
+      },
+      keyframes: {
+        move: {
+          '0%': { backgroundPosition: '0 0' },
+          '100%': { backgroundPosition: '100% 0' },
+        },
+        typing: {
+          "0%": { opacity: "0", transform: "translateY(10px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+        
+      },
+      animation: {
+        move: 'move 80s linear infinite', // Adjust timing and easing as needed
+        typing: "typing 0.5s ease forwards",
+      },
+    
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [
+    require("daisyui"),
+    require('tailwindcss-bg-patterns'),
+  ],
+  
   daisyui: {
     themes: ["light"],
   },
+  
+  
 } satisfies Config;
