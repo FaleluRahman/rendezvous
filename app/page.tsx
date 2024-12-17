e49053e6d3b401e96669ac8927a8a5a06e150b86
shameem <shameemalit96@gmail.com>
2024-12-17 15:21:45 +0530

    adeed full home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Header from "@/component/home/Header";
 import ImageSlider from "@/component/home/ImageSlider";
+import Shedule from "@/component/home/Shedule";
+import UpComing from "@/component/home/UpComing";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <ImageSlider />
+      <UpComing />
+      <Shedule />
       
     </div>
   );
