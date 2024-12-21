e0cef5090becd57bae3fd74a5653c1601e794a1b
falelu Rahman <tpfalelurahman@gmail.com>
2024-12-21 17:21:56 +0530

    upppp

--- component/events/Eventset.tsx
+++ component/events/Eventset.tsx
@@ -4,19 +4,19 @@ const Eventset = () => {
   return (
     <div className='bg-orange-50 flex justify-center items-center flex-col min-h-screen'>
     {/* event/1 */}
-    <div className='bg-white rounded-3xl w-[85%] flex flex-col mt-5'>
-      <div className='h-8 bg-white mt-3 text-zinc-700 mx-5 rounded-t-3xl text-lg flex items-center justify-between px-5 py-6 font-bold'>
-        <p>Expert Converse</p> <img className='h-6' src="/image/qr-code-scan.svg" alt="" />
+    <div className='bg-white rounded-3xl w-[85%] flex flex-col mt-5 px-5'>
+    <div className='h-8 bg-white mt-3 text-zinc-700  rounded-t-3xl text-lg flex  justify-between font-bold'>
+    <p>Expert Converse</p> <img className='h-6' src="/image/qr-code-scan.svg" alt="" />
       </div>
       <img 
-        className='rounded-3xl mx-auto w-[80%]' 
+        className='rounded-3xl  w-auto' 
         src="/image/712.jpg" 
         alt="wwwww" 
-      />
-      <p className='px-10 text-justify leading-tight pt-3'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+      /> 
+      <p className=' text-justify leading-tight pt-3'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
         Voluptatem a, libero quas atque consectetur iusto necessitatibus voluptate nesciunt ab perferendis dolore 
         tempora earum omnis iste minus corrupti voluptas vitae officiis.</p>
-        <div className='h-8 bg-white mt-5 text-zinc-700 mx-5 rounded-t-3xl mb-2 text-sm flex items-center justify-between px-5 font-bold'>
+        <div className='h-8 bg-white mt-5 text-zinc-700  rounded-t-3xl mb-2 text-sm flex items-center justify-between font-bold'>
         <div className='flex flex-col'>
         <div className='flex'><img className='h-4' src="/image/Timer.gif" alt="" /><p>09:30AM</p></div> 
         <div className='flex'><img className='h-4' src="/image/Location.gif" alt="" /><p>Conferance Hall</p></div>
@@ -27,19 +27,66 @@ const Eventset = () => {
     </div>
   
     {/* event/2 */}
-    <div className='bg-white rounded-3xl w-[85%] flex flex-col mt-5'>
-      <div className='h-8 bg-white mt-3 text-zinc-700 mx-5 rounded-t-3xl text-lg flex items-center justify-between px-5 font-bold'>
+    <div className='bg-white rounded-3xl w-[85%] flex flex-col mt-5 px-5'>
+      <div className='h-8 bg-white mt-3 text-zinc-700  rounded-t-3xl text-lg flex  justify-between font-bold'>
         <p>Expert Converse</p> <img className='h-6' src="/image/qr-code-scan.svg" alt="" />
       </div>
       <img 
-        className='rounded-3xl mx-auto w-[80%]' 
-        src="/image/300.jpg" 
+        className='rounded-3xl  w-auto' 
+        src="/image/301.jpg" 
         alt="wwwww" 
-      />
-      <p className='px-10 text-justify leading-tight pt-3'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+      /> 
+      <p className=' text-justify leading-tight pt-3'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
         Voluptatem a, libero quas atque consectetur iusto necessitatibus voluptate nesciunt ab perferendis dolore 
         tempora earum omnis iste minus corrupti voluptas vitae officiis.</p>
-      <div className='h-8 bg-white mt-5 text-zinc-700 mx-5 rounded-t-3xl mb-2 text-sm flex items-center justify-between px-5 font-bold'>
+        <div className='h-8 bg-white mt-5 text-zinc-700  rounded-t-3xl mb-2 text-sm flex items-center justify-between font-bold'>
+        <div className='flex flex-col'>
+        <div className='flex'><img className='h-4' src="/image/Timer.gif" alt="" /><p>09:30AM</p></div> 
+        <div className='flex'><img className='h-4' src="/image/Location.gif" alt="" /><p>Conferance Hall</p></div>
+        </div>
+
+        <div className='flex'><img className='h-10' src="/image/Dollar Donation.gif" alt="" /><p className='text-3xl mt-2'>5</p></div> 
+      </div>
+    </div>
+
+
+     {/* event/3 */}
+     <div className='bg-white rounded-3xl w-[85%] flex flex-col mt-5 px-5'>
+    <div className='h-8 bg-white mt-3 text-zinc-700  rounded-t-3xl text-lg flex  justify-between font-bold'>
+    <p>Expert Converse</p> <img className='h-6' src="/image/qr-code-scan.svg" alt="" />
+      </div>
+      <img 
+        className='rounded-3xl  w-auto' 
+        src="/image/505.jpg" 
+        alt="wwwww" 
+      /> 
+      <p className=' text-justify leading-tight pt-3'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+        Voluptatem a, libero quas atque consectetur iusto necessitatibus voluptate nesciunt ab perferendis dolore 
+        tempora earum omnis iste minus corrupti voluptas vitae officiis.</p>
+        <div className='h-8 bg-white mt-5 text-zinc-700  rounded-t-3xl mb-2 text-sm flex items-center justify-between font-bold'>
+        <div className='flex flex-col'>
+        <div className='flex'><img className='h-4' src="/image/Timer.gif" alt="" /><p>09:30AM</p></div> 
+        <div className='flex'><img className='h-4' src="/image/Location.gif" alt="" /><p>Conferance Hall</p></div>
+        </div>
+
+        <div className='flex'><img className='h-10' src="/image/Dollar Donation.gif" alt="" /><p className='text-3xl mt-2'>5</p></div> 
+      </div>
+    </div>
+  
+    {/* event/4 */}
+    <div className='bg-white rounded-3xl w-[85%] flex flex-col mt-5 px-5'>
+      <div className='h-8 bg-white mt-3 text-zinc-700  rounded-t-3xl text-lg flex  justify-between font-bold'>
+        <p>Expert Converse</p> <img className='h-6' src="/image/qr-code-scan.svg" alt="" />
+      </div>
+      <img 
+        className='rounded-3xl  w-auto' 
+        src="/image/300.jpg" 
+        alt="wwwww" 
+      /> 
+      <p className=' text-justify leading-tight pt-3'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+        Voluptatem a, libero quas atque consectetur iusto necessitatibus voluptate nesciunt ab perferendis dolore 
+        tempora earum omnis iste minus corrupti voluptas vitae officiis.</p>
+        <div className='h-8 bg-white mt-5 text-zinc-700  rounded-t-3xl mb-2 text-sm flex items-center justify-between font-bold'>
         <div className='flex flex-col'>
         <div className='flex'><img className='h-4' src="/image/Timer.gif" alt="" /><p>09:30AM</p></div> 
         <div className='flex'><img className='h-4' src="/image/Location.gif" alt="" /><p>Conferance Hall</p></div>
